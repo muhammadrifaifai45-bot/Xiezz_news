d3cdf7cb9dd77cc62c6c2af7afbe2a87ea206c21
rifai fai <Muhammadrifaifai45@gmail.comgit config --global user.name  Rifai>
2026-07-28 13:28:22 +0700

    add folder pages serta controller landing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
-  <link href="./css/output.css" rel="stylesheet">
+  <title>@yield('title')</title>
+  <link href={{ 'assets/css/output.css' }} rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
 
 <body>
   <div class="w-full">
     
-
+    @include('includes.navbar')
+    @yield('content')
    
 
    
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="/src/js/swiper.js"></script>
+    <script src={{ asset('assets/js/swiper.js') }}></script>
 </body>
 
 </html>
