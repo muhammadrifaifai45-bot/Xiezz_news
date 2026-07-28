691a538f91e252b26e345d767de12fce64b4e6c6
rifai fai <Muhammadrifaifai45@gmail.comgit config --global user.name  Rifai>
2026-07-28 11:53:25 +0700

    menambahkan folder includes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link href="./css/output.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+
+<body>
+  <div class="w-full">
+    
+
+   
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="/src/js/swiper.js"></script>
+</body>
+
+</html>
